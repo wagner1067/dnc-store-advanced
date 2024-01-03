44c60ea044fd972e4cdb27114da87ed00fd4c5a7
wagner1067 <barra-wag@hotmail.com>
2024-01-02 22:47:41 -0300

    para os cards destacar

---
+++ dnc-store/js/custom1.js
@@ -0,0 +1,9 @@
+function alternarDestaque () {
+	var cards = document.getElementsByClassName("card-produto");
+	cards = Array.from (cards);
+
+	for (let i = 0; i < cards.length; i++) {
+		cards[i].classList.toggle('card-produto-destaque');
+	}
+}
+setInterval(alternarDestaque, 1500)

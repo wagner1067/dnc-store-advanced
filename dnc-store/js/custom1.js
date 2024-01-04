e7e0bee44c7b5d11f447fe099b65654b877acd54
wagner1067 <barra-wag@hotmail.com>
2024-01-03 21:05:56 -0300

    aplicando função

--- dnc-store/js/custom1.js
+++ dnc-store/js/custom1.js
@@ -6,4 +6,25 @@ function alternarDestaque () {
 		cards[i].classList.toggle('card-produto-destaque');
 	}
 }
+
+function getProducts () {
+	fetch('http://localhost:3000/produtos').then((response) => response.json()).then((dados) => {
+		console.log(dados);
+
+		var list = document.getElementsById('product-list');
+		for(let i =0; i< dados.length; i++) {
+
+		list.innerHTML = list.innerHTML +  ` <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+		<div class="sport_product card-produto">
+		   <figure><img src="${dados[i].urlImagem}" alt="img" /></figure>
+		   <h3> $<strong class="price_text">${dados[i].preco}</strong></h3>
+		   <h4>${dados[i].nomeProduto}</h4>
+		</div>
+	 </div>`
+		}
+
+	})
+}
+
+getProducts.then ();
 setInterval(alternarDestaque, 1500)
